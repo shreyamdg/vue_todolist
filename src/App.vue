6e1ead39cf014132e2baeaf18d9b9a924f62ad23
shreyam15 <shreyam.duttagupta@gmail.com>
2019-03-11 23:11:13 -0400

    Added todo items with delete functionality

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Todos/>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
@@ -24,7 +24,7 @@ export default {
         {
           id:2,
           title: "Todo Two",
-          completed: false
+          completed: true
         },
         {
           id:3,
@@ -32,6 +32,11 @@ export default {
           completed: false
         }
       ]
+    }
+  },
+  methods: {
+    deleteTodo(id){
+      this.todos = this.todos.filter(todo => todo.id != id);
     }
   }
 }
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,12 +1,19 @@
 <template>
     <div>
-        <h1>Todos</h1>
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)"/>
+        </div>
     </div>
 </template>
 
 <script>
+import TodoItem from './TodoItem.vue'
 export default {
-    name: "Todos"
+    name: "Todos",
+    components:{
+        TodoItem
+    },
+    props: ["todos"]
 }
 </script>
 
